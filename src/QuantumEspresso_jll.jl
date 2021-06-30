34c79b6a1f485be1c842e68868801e20e044c3f8
jlbuild <juliabuildbot@gmail.com>
2021-06-30 13:38:45 +0000

    QuantumEspresso_jll build 6.7.0+0

---
+++ src/QuantumEspresso_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule QuantumEspresso_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("QuantumEspresso")
+JLLWrappers.@generate_main_file("QuantumEspresso", UUID("74603b90-2fcf-5710-a7d7-830b31b8b33c"))
+end  # module QuantumEspresso_jll
