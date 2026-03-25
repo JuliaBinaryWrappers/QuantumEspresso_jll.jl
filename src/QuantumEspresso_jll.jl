73784f6563a33b1c37aefaa0d5fd00523fa946ab
jlbuild <juliabuildbot@gmail.com>
2026-03-25 13:38:40 +0000

    QuantumEspresso_jll build 7.4.1+2

--- src/QuantumEspresso_jll.jl
+++ src/QuantumEspresso_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("QuantumEspresso")
-JLLWrappers.@generate_main_file("QuantumEspresso", UUID("74603b90-2fcf-5710-a7d7-830b31b8b33c"))
+JLLWrappers.@generate_main_file("QuantumEspresso", Base.UUID("74603b90-2fcf-5710-a7d7-830b31b8b33c"))
 end  # module QuantumEspresso_jll
